f03242379fa4c99f88e32f274c512c3398a706ed
luisgs7 <luisgsilva270@gmail.com>
2022-04-15 11:38:45 -0300

    config recipes views, the home function

---
+++ recipes/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(
+        request,
+        "recipes/home.html",
+        context={
+            "name": "pedro",
+        },
+    )
