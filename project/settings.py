b315f65014b8c18eb4af0dddb9c82be1c9873663
Luís Gustavo <luisgsilva270@gmail.com>
2022-04-18 14:13:32 +0000

    config STATIC_ROOT

--- project/settings.py
+++ project/settings.py
@@ -137,6 +137,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'templates/static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
