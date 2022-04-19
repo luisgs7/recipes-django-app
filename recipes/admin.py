c50f5c34a77c9b36029e58361a3b10552c4bc16e
luisgs7 <luisgsilva270@gmail.com>
2022-04-19 10:39:59 -0300

    config admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from recipes.models import Category
+from recipes.models import Category, Recipe
 
 
 class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
     ...
 
 
