fce4bcfa3897d78b076c42f49ce91429d1ed3f5b
luisgs7 <luisgsilva270@gmail.com>
2022-04-19 10:00:10 -0300

    model and migration recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 
 from recipes.views import home, recipes
 
-app_name = 'recipes'
+app_name = "recipes"
 
 urlpatterns = [
     path("", home, name="home"),
-    path("recipes/<int:id>/", recipes, name="detail")
+    path("recipes/<int:id>/", recipes, name="detail"),
 ]
