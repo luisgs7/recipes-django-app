30c7ddd580ca8614687ba6b16509f5f04c5ac19b
luisgs7 <luisgsilva270@gmail.com>
2022-04-19 16:44:09 -0300

    retirando a lib fake da view

--- recipes/views.py
+++ recipes/views.py
@@ -4,25 +4,23 @@ from .models import Recipe
 
 
 def home(request):
-    """recipes = Recipe.objects.all().order_by("-id")"""
+    recipes = Recipe.objects.all().order_by("-id")
 
     return render(
         request,
         "recipes/pages/home.html",
-        context={
-            "recipes": [make_recipe() for _ in range(10)],
-        },
+        context={"recipes": recipes},
     )
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by("-id")
+    recipe = Recipe.objects.filter(category__id=category_id).order_by("-id")
 
     return render(
         request,
         "recipes/pages/home.html",
         context={
-            "recipes": recipes,
+            "recipes": recipe,
         },
     )
 
