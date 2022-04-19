a3bf8e20466220cc5dec39f1ebd1cd36954aa017
luisgs7 <luisgsilva270@gmail.com>
2022-04-19 16:39:35 -0300

    config templates and lib fake

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from recipes.views import home, recipes
+from recipes.views import home, recipe, category
 
 app_name = "recipes"
 
 urlpatterns = [
     path("", home, name="home"),
-    path("recipes/<int:id>/", recipes, name="detail"),
+    path("recipes/category/<int:category_id>/", category, name="category"),
+    path("recipes/<int:id>/", recipe, name="recipe"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render
+from utils.recipes.fake import make_recipe
+from .models import Recipe
 
 
 def home(request):
+    """recipes = Recipe.objects.all().order_by("-id")"""
+
     return render(
         request,
         "recipes/pages/home.html",
         context={
-            "name": "pedro",
+            "recipes": [make_recipe() for _ in range(10)],
         },
     )
 
 
-def recipes(request, id):
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by("-id")
+
+    return render(
+        request,
+        "recipes/pages/home.html",
+        context={
+            "recipes": recipes,
+        },
+    )
+
+
+def recipe(request, id):
+
     return render(
         request,
         "recipes/pages/recipe-view.html",
         context={
+            "recipe": make_recipe(),
             "is_detail_page": True,
         },
     )
