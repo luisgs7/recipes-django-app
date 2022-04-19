3d13b97b2166a5bbe785951f0a95a22b31b017d9
luisgs7 <luisgsilva270@gmail.com>
2022-04-19 11:32:12 -0300

    new file comandos.sh

---
+++ comandos.sh
@@ -0,0 +1,30 @@
+# Django Shell
+
+python manage.py shell
+
+from recipes.models import Recipe, Category
+categories = Category.objects.all()
+categories[0]
+categories.order_by('-id')
+
+recipes = Recipe.objects.all()
+for recipe in recipes: print(recipe.id, recipe.title)
+for recipe in recipes.order_by('-id'): print(recipe.id, recipe.title)
+recipes.order_by('-id').first()
+recipe._meta.get_fields()
+recipe._meta.get_fields()[0]
+getattr(recipe, recipe._meta.get_fields()[0].name)
+getattr(recipe, 'id')
+
+new_category = Category()
+new_category.name = 'café da manhã'
+new_category.save()
+new_category.id
+
+new_category = Category.objects.create()
+new_category = Category.objects.create(name = 'Django Shell')
+
+consulta = Category.objects.get(id=2)
+consulta.delete()
+
+categories = Category.objects.filter(id=3)
