a0b61fa04c41d9d147880f81ee30d8234f660745
Luís Gustavo <luisgsilva270@gmail.com>
2022-04-26 13:09:42 +0000

    refactor test_recipe_base.py

--- recipes/tests/test_recipe_base.py
+++ recipes/tests/test_recipe_base.py
@@ -4,17 +4,53 @@ from recipes.models import Category, Recipe, User
 
 class RecipeTestBase(TestCase):
     def setUp(self) -> None:
-        category = Category.objects.create(name="Category")
-        author = User.objects.create_user(
-            first_name="user",
-            last_name="name",
-            username="username",
-            password="123456",
-            email="username@email.com",
+        return super().setUp()
+
+    def make_category(self, name="Category"):
+        return Category.objects.create(name=name)
+
+    def make_author(
+        self,
+        first_name="user",
+        last_name="name",
+        username="username",
+        password="123456",
+        email="username@email.com",
+    ):
+
+        return User.objects.create_user(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+            password=password,
+            email=email,
         )
-        recipe = Recipe.objects.create(  # noqa: F841
-            category=category,
-            author=author,
+
+    def make_recipe(
+        self,
+        category_data=None,
+        author_data=None,
+        title="Recipe Title",
+        description="Recipe Description",
+        slug="recipe-slug",
+        preparation_time=10,
+        preparation_time_unit="Minutos",
+        servings=5,
+        servings_unit="Porções",
+        preparation_steps="Recipe Preparation Steps",
+        preparation_steps_is_html=False,
+        is_published=True,
+    ):
+
+        if category_data is None:
+            category_data = {}
+
+        if author_data is None:
+            author_data = {}
+
+        return Recipe.objects.create(  # noqa: F841
+            category=self.make_category(**category_data),
+            author=self.make_author(**author_data),
             title="Recipe Title",
             description="Recipe Description",
             slug="recipe-slug",
@@ -26,5 +62,3 @@ class RecipeTestBase(TestCase):
             preparation_steps_is_html=False,
             is_published=True,
         )
-
-        return super().setUp()
